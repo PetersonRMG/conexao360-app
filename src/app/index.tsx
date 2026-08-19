2b87b6b91efac4ac6b059f54775e02811dfb49b7
PETERSON RICARDO MARCANDALLI GRACIANO <peterson.rmgraciano@senacsp.edu.br>
2026-08-19 08:33:13 -0300

    subindo o app conexao

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,98 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, View, Text, ImageBackground, Image, Pressable } from "react-native";
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
 
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
+export default function Index() {
   return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('@/assets/images/img/fundoLogin.png')}
+        style={styles.background}
+        resizeMode="stretch"
+
+      >
+
+        <View style={styles.conteudo}>
+          <View style={styles.conteudoTopo}>
+            <Image
+              source={require('@/assets/images/img/logo.svg')}>
+            </Image>
+            <Text style={styles.titulo}>Advocacia  {'\n'} E<Text style={styles.x}>x</Text>ponencial</Text>
+            <Text style={styles.subtitulo}>Rede Exclusiva</Text>
+          </View>
+          <View style={styles.conteudoBtns}>
+
+            <Pressable style={[styles.btnEntrar, styles.btn]}>Entrar</Pressable>
+            <Pressable style={[styles.btnCriar, styles.btn]}>Criar Conta</Pressable>
+            <Text style={styles.subtitulo}>Networking de alto nivel</Text>
+            <Text>Conhecimento <Text style={styles.x}>Exponencial</Text></Text>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
   );
 }
 
-export default function HomeScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
-
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: "#ffff"
+  },
+  background: {
+    width: '100%',
+    maxWidth: 440,
+    height: '100%',
+  },
+  conteudo: {
     flex: 1,
     justifyContent: 'center',
-    flexDirection: 'row',
+    alignItems: 'center',
   },
-  safeArea: {
+  conteudoTopo: {
     flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    margin:'auto',
+  },
+  conteudoBtns: {
     flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
+  titulo: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#ffff',
     textTransform: 'uppercase',
   },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
+  x: {
+    color: '#D6B26A',
   },
-});
+  subtitulo: {
+    fontSize: 20,
+    color: '#D6B26A',
+    fontStyle: 'normal',
+    textTransform: 'uppercase',
+  },
+  btnEntrar: {
+    color: '#2D1B33',
+    backgroundColor: '#D6B26A',
+
+
+  },
+  btnCriar: {
+    color: '#D6B26A',
+    backgroundColor: '#2D1B33',
+  },
+  btn: {
+    width: '90%',
+    borderRadius: 10,
+    margin: 'auto',
+    textAlign: 'center',
+    paddingBlock: 5,
+    marginBlock: 5,
+
+  }
+})
